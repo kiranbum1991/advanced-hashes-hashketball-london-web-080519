3b7f05d716c80c4372420856d8867cbeb05e3df6
Tommy Jake <tomlom1984@hotmail.co.uk>
2019-08-07 06:16:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -120,8 +120,8 @@ def num_points_scored(players_name)
   game_hash.each do |place, team|
     team.each do |attribute, data|
       if attribute == :players
-        binding.pry
         data.each do |player|
+          binding.pry
           if player[:players_name] == players_name
             return player[:points]
           end
