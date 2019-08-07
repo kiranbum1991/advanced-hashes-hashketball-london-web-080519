b66d09900d916b2b475b084ead54322e730dbf22
Tommy Jake <tomlom1984@hotmail.co.uk>
2019-08-07 06:00:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -118,8 +118,8 @@ end
 
 def num_points_scored (players_name)
   game_hash.each do |place, team|
-    binding.pry
-    team.each do ||
+    team.each do |attribute, data|
+      binding.pry
     end
   end
 end
